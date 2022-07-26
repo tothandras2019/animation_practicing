24a703f84746c9a1c87305fb16d05c33af900cb8
Andras <toth.andras.2000@gmail.com>
2022-07-26 09:44:33 +0200

    Mouse follow rough ready

--- frontend/mousefollow.js
+++ frontend/mousefollow.js
@@ -3,24 +3,25 @@
 const dotCircle = document.querySelector(`.dot__circle`);
 const dotInner = dotCircle.querySelector(`.dot__inner`);
 
-dotCircle.addEventListener(`mousemove`, OnMouseMove.bind(dotCircle));
+dotCircle.addEventListener(`mouseenter`, OnMouseMove.bind(dotInner));
+dotCircle.addEventListener(`mousemove`, OnMouseMove.bind(dotInner));
 dotCircle.addEventListener(`mouseleave`, OnMouseLeave);
 
 function OnMouseMove(event) {
-  let actualWidth = window.getComputedStyle(this).width.slice(0, -2);
-  let actualHeight = window.getComputedStyle(this).height.slice(0, -2);
+  console.log(event);
+  let moveToX = event.offsetX;
+  let moveToY = event.offsetY;
 
-  let moveToX = event.clientX * 0.2;
-  let moveToY = event.clientY * 0.2;
-
-  dotInner.style.setProperty(`--top-width`, `${moveToX}%`);
-  dotInner.style.setProperty(`--top-height`, `${moveToY}%`);
-
-  console.log(`X: `, event.clientX / actualWidth);
-  console.log(`Y`, event.clientY / actualHeight);
+  dotInner.style.setProperty(`top`, `${moveToY}%`);
+  dotInner.style.setProperty(`left`, `${moveToX}%`);
+  dotInner.style.setProperty(
+    `transform`,
+    `translate(-${moveToX}%, -${moveToY}%)`,
+  );
 }
 
 function OnMouseLeave(event) {
-  dotInner.style.setProperty(`--top-width`, `50%`);
-  dotInner.style.setProperty(`--top-height`, `50%`);
+  dotInner.style.setProperty(`top`, `50%`);
+  dotInner.style.setProperty(`left`, `50%`);
+  dotInner.style.setProperty(`transform`, `translate(-50%, -50%)`);
 }
