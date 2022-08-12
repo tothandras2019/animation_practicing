2440864817b79688188353c00e4d220211e685b5
Andras <toth.andras.2000@gmail.com>
2022-08-12 11:55:33 +0200

    rotating button ready

--- frontend/rotatingborder.js
+++ frontend/rotatingborder.js
@@ -1,3 +1,17 @@
 `use strict`;
 
 console.log(`done`);
+
+const root = document.getElementById(`root`);
+
+const buttonAll = root.querySelectorAll(`.start`);
+
+buttonAll.forEach((button) => {
+  button.addEventListener("mousedown", function (event) {
+    this.classList.add(`pressed`);
+  });
+
+  button.addEventListener("mouseup", function (event) {
+    this.classList.remove(`pressed`);
+  });
+});
